388716c2956f01fdbcc7b32db6b2f9c8bd6f5c27
Efin Muttaqin <emuttaqin@rekam.org>
2026-04-20 16:45:56 +0000

    fix: incomplete hello message

--- src/hello.py
+++ src/hello.py
@@ -1,4 +1,7 @@
 print("Hello World!")
 print("This is a simple Python script.")
-print("You can run this script to see the output.") 
+print("You can run this script to see the output.")
+print("Feel free to modify this script and experiment with Python!")
+
+
 
